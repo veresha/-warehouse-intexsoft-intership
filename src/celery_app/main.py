3347a0abe31aef2192980aaa149c8a3a9b8eb2ec
Eugene Vereshchagin <veresha23@gmail.com>
2023-01-13 18:18:30 +0300

    DONE

--- src/celery_app/main.py
+++ src/celery_app/main.py
@@ -1,11 +1,12 @@
 import os
 
 from celery import Celery
+from src.app.config import *
 
 app = Celery(
     'worker',
-    broker=os.environ.get("RABBITMQ_BROKER", "amqp://guest:guest@localhost:5672/"),
-    backend=os.environ.get("RABBITMQ_BACKEND", "rpc://"),
+    broker=RABBITMQ_BROKER,
+    backend=RABBITMQ_BACKEND,
     include=['src.celery_app.tasks']
 )
 app.autodiscover_tasks()
